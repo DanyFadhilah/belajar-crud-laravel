0bf61dab3535e530932286af465061c78615834b
DanyFadhilah <fadilahdany@gmail.com>
2019-11-18 17:53:18 +0700

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\facades\DB;
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = \App\User::all();
+
+        return view('index', compact('users'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = new user;
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->age = $request->age;
+        $user->born = $request->born;
+        $user->hobby = $request->hobby;
+        $user->address = $request->address;
+        $user->password = $request->password;
+
+        $user->save();
+
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        //
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,62 @@
+@extends("layouts.main")
+
+@section("title","User Data")
+
+@section("content")
+
+<div class="container">
+  <div class="row pt-5">
+    <div class="input-group float-right">
+        <h3 class="float-left">User's Data</h3>
+
+       <form class="form-inline my-2 my-lg-0 ml-auto" method="get" action="/search">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <a href="/create" class="btn btn-primary ml-2">Create Data</a>
+        </form>
+    </div>
+    <table class="table border mt-3">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">Password</th>
+          <th scope="col">Address</th>  
+          <th scope="col">Age</th>  
+          <th scope="col">Born</th>  
+          <th scope="col">Hobby</th>  
+          <th scope="col">Action</th>  
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($users as $user)
+          <tr>
+            <th scope="row">{{ $loop->iteration }}</th>
+            <td>{{ $user->name }}</td>
+            <td>{{ $user->email }}</td>
+            <td>{{ $user->password }}</td>
+            <td>{{ $user->address }}</td>
+            <td>{{ $user->age }}</td>
+            <td>{{ $user->born }}</td>  
+            <td>{{ $user->hobby }}</td>
+            <td>
+              <a href="/user/{{ $user->id }}/edit" class="badge badge-primary">Edit</a>
+               <a  href="/delete/{{ $user->id }}" class="badge badge-danger">Delete</a>
+          </td>
+          </tr>
+      @endforeach
+      </tbody>
+    </table>
+  </div>
+</div>
+
+@endsection
+
+@section("js")
+
+@endsection
+
+@section("css")
+
+@endsection
